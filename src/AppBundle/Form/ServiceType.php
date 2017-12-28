7c30a0f19362af43dcd2657bd8c98ec60abbc6d4
janssens <janssensgaetan@gmail.com>
2017-12-28 12:46:38 +0100

    basic CRU on services

---
+++ src/AppBundle/Form/ServiceType.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Beneficiary;
+use AppBundle\Entity\Task;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class ServiceType extends AbstractType
+{
+    private $tokenStorage;
+
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        // grab the user, do a quick sanity check that one exists
+        $user = $this->tokenStorage->getToken()->getUser();
+        if (!$user) {
+            throw new \LogicException(
+                'cannot be used without an authenticated user!'
+            );
+        }
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) use ($user) {
+            $form = $event->getForm();
+            $userData = $event->getData();
+
+            $form->add('name',TextType::class,array('label'=>'nom','required' => true,))
+                ->add('description',TextType::class,array('label'=>'Description'))
+                ->add('url',TextType::class,array('label'=>'Adresse web'));
+        });
+
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Service'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_service';
+    }
+
+
+}
