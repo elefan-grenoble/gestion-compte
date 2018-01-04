f9db2b3538f301f8345496484113fd22cb451cf3
Gaëtan Janssens on KIRA <janssensgaetan@gmail.com>
2018-01-04 20:31:56 +0100

    merge error

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -524,31 +524,6 @@ class User extends BaseUser
         return $this->frozen;
     }
 
-
-    /**
-     * Set lastRegistration
-     *
-     * @param \AppBundle\Entity\Registration $lastRegistration
-     *
-     * @return User
-     */
-    public function setLastRegistration(\AppBundle\Entity\Registration $lastRegistration = null)
-    {
-        $this->lastRegistration = $lastRegistration;
-
-        return $this;
-    }
-
-    /**
-     * Get lastRegistration
-     *
-     * @return \AppBundle\Entity\Registration
-     */
-    public function getLastRegistration()
-    {
-        return $this->lastRegistration;
-    }
-
     /**
      * determine whether the given client (ClientInterface) is allowed by the user, or not.
      * @param ClientInterface $client
