3cdc168e72088ec81e67fe6424b3f043bff88bc6
janssens <janssensgaetan@gmail.com>
2018-10-06 10:59:28 +0200

    local only file

--- restart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "truncate db"
-sudo mysql -uroot -e "DROP DATABASE membres_new_schema;"
-sudo mysql -uroot -e "CREATE DATABASE membres_new_schema;"
-echo "import dump"
-mysql -ubabar -ppassword membres_new_schema < dump-espace-membres.sql
-echo "migrate data"
-mysql -ubabar -ppassword membres_new_schema < migrate-to-multi-user.sql
