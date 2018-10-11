132dcdea677be0d4aca809854be6ea3140db8209
Bastien Deshayes <bastien.deshayes@kelkoogroup.com>
2018-10-11 10:39:19 +0200

    Clean script de migration (beneficiary_id)

--- migrate-to-multi-user.sql
+++ migrate-to-multi-user.sql
@@ -11,13 +11,9 @@ ALTER TABLE beneficiary ADD CONSTRAINT FK_7ABF446A1FB354CD FOREIGN KEY (membersh
 CREATE INDEX IDX_7ABF446A1FB354CD ON beneficiary (membership_id);
 UPDATE beneficiary set membership_id = user_id;
 
--- fos_user / Replace main_beneficiary_id by beneficiary_id
+-- fos_user / Delete main_beneficiary_id
 ALTER TABLE fos_user DROP FOREIGN KEY FK_957A64796986CF73;
-ALTER TABLE fos_user ADD beneficiary_id INT DEFAULT NULL;
-ALTER TABLE fos_user ADD CONSTRAINT FK_957A6479ECCAAFA0 FOREIGN KEY (beneficiary_id) REFERENCES beneficiary (id) ON DELETE SET NULL;
 ALTER TABLE fos_user ADD CONSTRAINT FK_957A64796986CF73 FOREIGN KEY (last_registration_id) REFERENCES registration (id);
-CREATE UNIQUE INDEX UNIQ_957A6479ECCAAFA0 ON fos_user (beneficiary_id);
-UPDATE fos_user SET beneficiary_id = main_beneficiary_id;
 
 -- Migrate time_logs
 ALTER TABLE time_log ADD membership_id INT NOT NULL;
@@ -28,6 +24,10 @@ CREATE INDEX IDX_55BE03AF1FB354CD ON time_log (membership_id);
 ALTER TABLE time_log DROP FOREIGN KEY FK_55BE03AFA76ED395;
 DROP INDEX IDX_55BE03AFA76ED395 ON time_log;
 ALTER TABLE time_log DROP user_id;
+
+-- beneficiary/user relationship / OneToMany to ManyToOne
+UPDATE beneficiary b JOIN fos_user u ON u.id = b.user_id SET b.user_id = NULL WHERE b.id != u.main_beneficiary_id;
+ALTER TABLE beneficiary DROP INDEX IDX_7ABF446AA76ED395, ADD UNIQUE INDEX UNIQ_7ABF446AA76ED395 (user_id);
 
 -- Migrate registrations
 ALTER TABLE registration ADD membership_id INT DEFAULT NULL;
@@ -43,9 +43,27 @@ UPDATE note SET membership_id = user_id;
 ALTER TABLE note ADD CONSTRAINT FK_CFBDFA141FB354CD FOREIGN KEY (membership_id) REFERENCES membership (id);
 CREATE INDEX IDX_CFBDFA141FB354CD ON note (membership_id);
 
--- beneficiary/user relationship / OneToMany to ManyToOne
-UPDATE beneficiary b JOIN fos_user u ON u.id = b.user_id SET b.user_id = NULL WHERE b.id != u.main_beneficiary_id;
-ALTER TABLE beneficiary DROP INDEX IDX_7ABF446AA76ED395, ADD UNIQUE INDEX UNIQ_7ABF446AA76ED395 (user_id);
+-- Migrate address
+ALTER TABLE beneficiary ADD address_id INT DEFAULT NULL;
+UPDATE beneficiary b JOIN fos_user u ON u.id = b.user_id SET b.address_id = u.address_id;
+ALTER TABLE beneficiary ADD CONSTRAINT FK_7ABF446AF5B7AF75 FOREIGN KEY (address_id) REFERENCES address (id);
+CREATE UNIQUE INDEX UNIQ_7ABF446AF5B7AF75 ON beneficiary (address_id);
+
+-- Migrate proxies
+ALTER TABLE proxy DROP FOREIGN KEY FK_7372C9BE5DE37FD9;
+ALTER TABLE proxy ADD CONSTRAINT FK_7372C9BE5DE37FD9 FOREIGN KEY (giver) REFERENCES membership (id) ON DELETE CASCADE;
+
+
+-- Drop old columns of user
+ALTER TABLE fos_user DROP FOREIGN KEY FK_957A647962C6E4EA;
+ALTER TABLE fos_user DROP FOREIGN KEY FK_957A64796986CF73;
+ALTER TABLE fos_user DROP FOREIGN KEY FK_957A6479F5B7AF75;
+
+DROP INDEX UNIQ_957A647962C6E4EA ON fos_user;
+DROP INDEX UNIQ_957A6479F5B7AF75 ON fos_user;
+DROP INDEX UNIQ_957A64796986CF73 ON fos_user;
+
+ALTER TABLE fos_user DROP address_id, DROP last_registration_id, DROP withdrawn, DROP frozen, DROP first_shift_date, DROP frozen_change, DROP member_number;
 
 -- Move email from beneficiary to user
 CREATE TABLE `newuser` (
@@ -141,29 +159,9 @@ DROP TABLE newuser;
 DROP INDEX UNIQ_7ABF446AE7927C74 ON beneficiary;
 ALTER TABLE beneficiary DROP email;
 
+ALTER TABLE fos_user DROP main_beneficiary_id;
 
--- Migrate address
-ALTER TABLE beneficiary ADD address_id INT DEFAULT NULL;
-UPDATE beneficiary b JOIN fos_user u ON u.id = b.user_id SET b.address_id = u.address_id;
-ALTER TABLE beneficiary ADD CONSTRAINT FK_7ABF446AF5B7AF75 FOREIGN KEY (address_id) REFERENCES address (id);
-CREATE UNIQUE INDEX UNIQ_7ABF446AF5B7AF75 ON beneficiary (address_id);
-
--- Migrate proxies
-ALTER TABLE proxy DROP FOREIGN KEY FK_7372C9BE5DE37FD9;
-ALTER TABLE proxy ADD CONSTRAINT FK_7372C9BE5DE37FD9 FOREIGN KEY (giver) REFERENCES membership (id) ON DELETE CASCADE;
-
--- Drop old columns
-ALTER TABLE fos_user DROP FOREIGN KEY FK_957A647962C6E4EA;
-ALTER TABLE fos_user DROP FOREIGN KEY FK_957A64796986CF73;
-ALTER TABLE fos_user DROP FOREIGN KEY FK_957A6479F5B7AF75;
-ALTER TABLE fos_user DROP FOREIGN KEY FK_957A6479ECCAAFA0;
-
-DROP INDEX UNIQ_957A647962C6E4EA ON fos_user;
-DROP INDEX UNIQ_957A6479F5B7AF75 ON fos_user;
-DROP INDEX UNIQ_957A64796986CF73 ON fos_user;
-DROP INDEX UNIQ_957A6479ECCAAFA0 ON fos_user;
-
-ALTER TABLE fos_user DROP main_beneficiary_id, DROP address_id, DROP last_registration_id, DROP withdrawn, DROP frozen, DROP first_shift_date, DROP frozen_change, DROP member_number, DROP beneficiary_id;
+-----
 
 ALTER TABLE registration DROP FOREIGN KEY FK_62A8A7A7A76ED395;
 DROP INDEX IDX_62A8A7A7A76ED395 ON registration;
