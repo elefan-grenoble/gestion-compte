982a0cba5a8f384082167410fe77413a5b89b22f
Yoann Laissus <yoann.laissus@gmail.com>
2018-12-09 11:30:18 +0100

    Fix Formation form validation

--- src/AppBundle/Form/FormationType.php
+++ src/AppBundle/Form/FormationType.php
@@ -4,7 +4,7 @@ namespace AppBundle\Form;
 
 use AppBundle\Entity\Formation;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,7 +15,7 @@ class FormationType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name');
+        $builder->add('name', TextType::class, array('label' => 'Nom'));
     }
     
     /**
