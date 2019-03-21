fcf12197f50dc8182371bc9020b77007695ae9e5
Bastien Deshayes <bastien.deshayes@kelkoogroup.com>
2019-03-21 11:47:13 +0100

    WIP Création manuelle de créneau

---
+++ src/AppBundle/Form/ShiftType.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Shift;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use AppBundle\Form\ShiftMapper;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\DataMapperInterface;
+
+class ShiftType extends AbstractType implements DataMapperInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date', TextType::class, array('label' => 'Date', 'attr' => array('class' => 'datepicker')))
+            ->add('start', TextType::class, array('label' => 'Heure de début', 'attr' => array('class' => 'timepicker')))
+            ->add('end', TextType::class, array('label' => 'Heure de fin', 'attr' => array('class' => 'timepicker')))
+            ->add('formation', EntityType::class, array(
+                'label' => 'Formation',
+                'class' => 'AppBundle:Formation',
+                'choice_label' => 'name',
+                'multiple' => false,
+                'required' => false
+            ))
+            ->add('job', EntityType::class, array(
+                'label' => 'Poste',
+                'class' => 'AppBundle:Job',
+                'choice_label' => 'name',
+                'multiple' => false,
+                'required' => true
+            ))
+            ->setDataMapper($this);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_shift';
+    }
+
+    /**
+     * @param Shift|null $data
+     */
+    public function mapDataToForms($data, $forms)
+    {
+        // there is no data yet, so nothing to prepopulate
+        if (null === $data) {
+            return;
+        }
+
+        // invalid data type
+        if (!$data instanceof Shift) {
+            throw new UnexpectedTypeException($data, Shift::class);
+        }
+
+        /** @var FormInterface[] $forms */
+        $forms = iterator_to_array($forms);
+
+        // initialize form field values
+        $forms['date']->setData($data->getStart());
+        $forms['start']->setData($data->getStart());
+        $forms['end']->setData($data->getEnd());
+        $forms['job']->setData($data->getJob());
+        $forms['formation']->setData($data->getFormation());
+    }
+
+    public function mapFormsToData($forms, &$data)
+    {
+        /** @var FormInterface[] $forms */
+        $forms = iterator_to_array($forms);
+
+        // as data is passed by reference, overriding it will change it in
+        // the form object as well
+        // beware of type inconsistency, see caution below
+        $data = new Shift();
+
+        $date = new \DateTime($forms['date']->getData());
+        $start = new \DateTime($forms['start']->getData());
+        $end = new \DateTime($forms['end']->getData());
+
+        $year = intval($date->format('Y'));
+        $month = intval($date->format('M'));
+        $day = intval($date->format('d'));
+
+        $start->setDate($year, $month, $day);
+        $end->setDate($year, $month, $day);
+
+        $data->setStart($start);
+        $data->setEnd($end);
+        $data->setJob($forms['job']->getData());
+        $data->setFormation($forms['formation']->getData());
+
+        die($data);
+    }
+}
