6227961f706ab259a4e1120141181dd06d5c89f5
janssens <janssensgaetan@gmail.com>
2019-08-03 03:06:11 +0200

    more filter to work with process update in twig

--- src/AppBundle/Twig/Extension/ProcessUpdateExtension.php
+++ src/AppBundle/Twig/Extension/ProcessUpdateExtension.php
@@ -7,6 +7,7 @@ use AppBundle\Entity\Shift;
 use Symfony\Component\DependencyInjection\Container;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+use Twig\TwigFunction;
 
 class ProcessUpdateExtension extends AbstractExtension
 {
@@ -21,7 +22,9 @@ class ProcessUpdateExtension extends AbstractExtension
     {
         return array(
             new TwigFilter('last_shift_date',array($this,'last_shift_date')),
-            new TwigFilter('updates_list_from_date',array($this,'updates_list_from_date'))
+            new TwigFilter('updates_list_from_date',array($this,'updates_list_from_date')),
+            new TwigFilter('count_updates_list_from_date',array($this,'count_updates_list_from_date')),
+            new TwigFilter('w3c_to_date',array($this,'w3c_to_date'))
         );
     }
 
@@ -31,5 +34,13 @@ class ProcessUpdateExtension extends AbstractExtension
 
     public function updates_list_from_date(\DateTime $date){
         return $this->container->get('doctrine')->getManager()->getRepository(ProcessUpdate::class)->findFrom($date);
+    }
+
+    public function count_updates_list_from_date(\DateTime $date){
+        return $this->container->get('doctrine')->getManager()->getRepository(ProcessUpdate::class)->countFrom($date);
+    }
+
+    public function w3c_to_date($w3c){
+        return \DateTime::createFromFormat(\DateTimeInterface::W3C,$w3c);
     }
 }
