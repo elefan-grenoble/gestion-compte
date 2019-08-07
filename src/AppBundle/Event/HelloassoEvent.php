29e213bd652bd465f25b4217ebab0caa9ce3325d
janssens <janssensgaetan@gmail.com>
2019-08-07 19:51:15 +0200

    Autogestion des problèmes Helloasso

--- src/AppBundle/Event/HelloassoEvent.php
+++ src/AppBundle/Event/HelloassoEvent.php
@@ -12,24 +12,32 @@
 namespace AppBundle\Event;
 
 use AppBundle\Entity\HelloassoPayment;
+use AppBundle\Entity\User;
 use Symfony\Component\EventDispatcher\Event;
 
 class HelloassoEvent extends Event
 {
     const PAYMENT_AFTER_SAVE = 'helloasso.payment_after_save';
+    const ORPHAN_SOLVE = 'helloasso.orphan_solve';
 
     /**
      * @var HelloassoPayment
      */
     private $payment;
+    /**
+     * @var User
+     */
+    private $user;
 
 
     /**
      * @param HelloassoPayment $payment
+     * @param User $user
      */
-    public function __construct(HelloassoPayment $payment)
+    public function __construct(HelloassoPayment $payment,User $user = null)
     {
         $this->payment = $payment;
+        $this->user = $user;
     }
 
     /**
@@ -40,4 +48,11 @@ class HelloassoEvent extends Event
         return $this->payment;
     }
 
+    /**
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
 }
