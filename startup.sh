b3645dac987720385bd6cead593ffda716eb3978
jeremy <jeremy@wenjy.fr>
2020-08-10 21:35:23 +0200

    Ajout du startup.sh qui manquait en prod.

--- startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-php /elefan/bin/console doctrine:migration:migrate
-php /elefan/bin/console cache:warmup
-if [ -n "$SUPER_ADMIN_USERNAME" ];
-then
-  php /elefan/bin/console fos:user:change-password "$SUPER_ADMIN_USERNAME" "$SUPER_ADMIN_PASSWORD"
-fi
