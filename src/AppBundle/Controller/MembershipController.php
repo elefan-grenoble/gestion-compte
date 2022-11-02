elefan-grenoble/gestion-compte

commit 618f34b320d46a1d9def10dcd4e98e3abf0819ff

Admin : refactoring de ROLE_USER_VIEWER (#507)
* Give ROLE_USER_VIEWER access to ROLE_ADMIN_PANEL

* Make office_tools only accessible to ROLE_USER_VIEWER

* Move Adhésion button to /admin page

* Add Administration to breadcrumbs

* Use ré-adhérer wording. Add security on some pages

* Cleanup useless denyAccessUnlessGranted

src/AppBundle/Controller/MembershipController.php
@@ -201,7 +201,7 @@ class MembershipController extends Controller
             }
 
             if ($this->getCurrentAppUser()->getBeneficiary() && $this->getCurrentAppUser()->getBeneficiary()->getMembership()->getId() == $member->getId()) {
-                $session->getFlashBag()->add('error', 'Tu ne peux pas enregistrer ta propre réadhésion, demande à un autre adhérent :)');
+                $session->getFlashBag()->add('error', 'Tu ne peux pas enregistrer ta propre ré-adhésion, demande à un autre adhérent :)');
                 return $this->redirectToShow($member);
             }
             $newReg->setRegistrar($this->getCurrentAppUser());
@@ -329,6 +329,7 @@ class MembershipController extends Controller
      *
      * @Route("/edit", name="member_edit_firewall")
      * @Method({"GET", "POST"})
+     * @Security("has_role('ROLE_USER_VIEWER')")
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
      */
@@ -336,18 +337,18 @@ class MembershipController extends Controller
     {
         $session = new Session();
 
-        if ($this->isGranted('view', new User())) {
+        if ($this->isGranted('ROLE_USER_VIEWER')) {
             $form = $this->createFormBuilder()
                 ->add('member_number', IntegerType::class, array('label' => 'Numéro d\'adhérent'))
                 ->add('username', HiddenType::class, array('attr' => array('value' => '')))
                 ->add('email', HiddenType::class, array('label' => 'email'))
                 ->add('edit', SubmitType::class, array('label' => 'Editer', 'attr' => array('class' => 'btn')))
                 ->getForm();
-        } else {
+        } else {  # higher privileges
             $form = $this->createFormBuilder()
                 ->add('member_number', IntegerType::class, array('label' => 'Numéro d\'adhérent'))
                 ->add('username', HiddenType::class, array('attr' => array('value' => '')))
-                ->add('email', EmailType::class, array('label' => 'email'))
+                ->add('email', EmailType::class, array('label' => 'email'))  # visible
                 ->add('edit', SubmitType::class, array('label' => 'Editer', 'attr' => array('class' => 'btn')))
                 ->getForm();
         }
@@ -606,13 +607,11 @@ class MembershipController extends Controller
     }
 
     /**
-     * Creates a new membership entity.
+     * Creates a new membership entity
      *
      * @Route("/new", name="member_new")
      * @Method({"GET", "POST"})
-     * @param Request $request
-     * @return Response
-     * @throws
+     * @Security("has_role('ROLE_SUPER_ADMIN')")
      */
     public function newAction(Request $request)
     {
@@ -621,16 +620,16 @@ class MembershipController extends Controller
         $code = $request->query->get('code');
         $em = $this->getDoctrine()->getManager();
         $a_beneficiary = null;
-        if ($code){
+        if ($code) {
             $email = $this->get('AppBundle\Helper\SwipeCard')->vigenereDecode($code);
-            if ($email){
+            if ($email) {
                 $a_beneficiary = $em->getRepository('AppBundle:AnonymousBeneficiary')->findOneBy(array('email'=>$email));
             }
-            if (!$a_beneficiary){
+            if (!$a_beneficiary) {
                 $session->getFlashBag()->add('error', 'Cette url n\'est plus valide');
                 return $this->redirectToRoute("homepage");
-            }else{
-                if ($a_beneficiary->getJoinTo()){ //adding beneficiary to an existing membership : wrong place
+            } else {
+                if ($a_beneficiary->getJoinTo()) { //adding beneficiary to an existing membership : wrong place
                     return $this->redirectToRoute('member_add_beneficiary', array('code' => $this->container->get('AppBundle\Helper\SwipeCard')->vigenereEncode($email)));
                 }
             }
@@ -641,7 +640,7 @@ class MembershipController extends Controller
         }
 
         $member = new Membership();
-        if ($a_beneficiary){
+        if ($a_beneficiary) {
             $user = new User();
             $user->setEmail($a_beneficiary->getEmail());
             $beneficiary = new Beneficiary();
@@ -657,15 +656,15 @@ class MembershipController extends Controller
         $member->setMemberNumber($mm);
 
         $registration = new Registration();
-        if ($a_beneficiary){
+        if ($a_beneficiary) {
             $registration->setDate($a_beneficiary->getCreatedAt());
             $registration->setRegistrar($a_beneficiary->getRegistrar());
             $registration->setAmount($a_beneficiary->getAmount());
             $registration->setMode($a_beneficiary->getMode());
-            if ($a_beneficiary->getMode()===Registration::TYPE_HELLOASSO){
+            if ($a_beneficiary->getMode()===Registration::TYPE_HELLOASSO) {
                 $registration->setAmount('--');
             }
-        }else{
+        } else {
             $registration->setDate(new DateTime('now'));
             $registration->setRegistrar($this->getUser());
         }
@@ -888,14 +887,14 @@ class MembershipController extends Controller
     }
 
     /**
-     * Lists all user entities.
+     * Office tools: membership creation & management
      *
      * @Route("/office_tools", name="user_office_tools")
      * @Method({"GET","POST"})
+     * @Security("has_role('ROLE_USER_VIEWER')")
      */
     public function officeToolsAction(Request $request)
     {
-        $this->denyAccessUnlessGranted('access_tools', $this->getCurrentAppUser());
         $note = new Note();
         $note->setAuthor($this->getCurrentAppUser());
         $note_form = $this->createForm(NoteType::class, $note);
@@ -919,7 +918,6 @@ class MembershipController extends Controller
             }
         }
 
-
         $notes = $em->getRepository('AppBundle:Note')->findBy(array("subject" => null));
         $notes_form = array();
         $notes_delete_form = array();
@@ -935,6 +933,7 @@ class MembershipController extends Controller
 
             $new_notes_form[$n->getId()] = $response_note_form->createView();
         }
+
         return $this->render('default/tools/office_tools.html.twig', array(
             'note_form' => $note_form->createView(),
             'notes_form' => $notes_form,

app/Resources/views/admin/index.html.twig
@@ -15,6 +15,13 @@
                 <i class="material-icons">build</i>&nbsp;Administration
             </h4>
 
+            {% if is_granted("ROLE_USER_VIEWER") %}
+                <h6><i class="material-icons">person_add</i>&nbsp;Adhésions</h6>
+                <a href="{{ path("user_office_tools") }}" class="waves-effect waves-light teal btn">
+                    <i class="material-icons left">folder_shared</i>Adhésion / ré-adhésion
+                </a>
+            {% endif %}
+
             {% if is_granted("ROLE_USER_MANAGER") %}
                 <h6><i class="material-icons">people</i>&nbsp;Membres</h6>
                 <a href="{{ path("user_index") }}" class="waves-effect waves-light btn">
@@ -36,7 +43,7 @@
             {% if is_granted("ROLE_FINANCE_MANAGER") %}
                 <h6><i class="material-icons">attach_money</i>&nbsp;Finances</h6>
                 <a href="{{ path("registrations") }}" class="waves-effect waves-light btn indigo">
-                    <i class="material-icons left">attach_money</i>Gérer les Adhésions / Réadhésions
+                    <i class="material-icons left">attach_money</i>Gérer les Adhésions / Ré-adhésions
                 </a>
                 <a href="{{ path("helloasso_payments") }}" class="waves-effect waves-light btn indigo">
                     <i class="material-icons left">settings_ethernet</i>Paiements helloasso

app/Resources/views/admin/pre_user/list.html.twig
@@ -1,9 +1,10 @@
 {% extends 'layout.html.twig' %}
 
 {% block breadcrumbs %}
-    <a href="{{ path('homepage') }}"><i class="material-icons">home</i></a><i class="material-icons">chevron_right</i>
-    <a href="{{ path('user_office_tools') }}"><i class="material-icons">build</i><span class="hide-on-small-and-down">Outils adhésion et ré-adhésion</span></a><i class="material-icons">chevron_right</i>
-    <i class="material-icons">list</i>Liste des adhésions en attente
+<a href="{{ path('homepage') }}"><i class="material-icons">home</i></a><i class="material-icons">chevron_right</i>
+<a href="{{ path('admin') }}"><i class="material-icons">build</i>&nbsp;Administration</a><i class="material-icons">chevron_right</i>
+<a href="{{ path('user_office_tools') }}"><i class="material-icons">folder_shared</i>&nbsp;Adhésion et ré-adhésion</a><i class="material-icons">chevron_right</i>
+<i class="material-icons">list</i>&nbsp;Liste des adhésions en attente
 {% endblock %}
 
 {% block stylesheets %}
@@ -75,7 +76,7 @@
 
                 <td>
                     {% if is_granted("ROLE_USER_MANAGER") %}
-                        <a href="{{ path('user_quick_new_recall',{'id' : anonymousBeneficiary.id }) }}" class="btn waves-effect waves-light green darken-2"><i class="material-icons left">redo</i> Relancer</a>
+                        <a href="{{ path('user_quick_new_recall', { 'id' : anonymousBeneficiary.id }) }}" class="btn waves-effect waves-light green darken-2"><i class="material-icons left">redo</i> Relancer</a>
                         <br> <a href="{{ path('pre_user_delete', { 'id': anonymousBeneficiary.id }) }}" class="btn red"><i class="material-icons left">delete</i>Supprimer</a>
                     {% endif %}
                 </td>

app/Resources/views/ambassador/phone/list.html.twig
@@ -2,7 +2,8 @@
 
 {% block breadcrumbs %}
 <a href="{{ path('homepage') }}"><i class="material-icons">home</i></a><i class="material-icons">chevron_right</i>
-<a href="{{ path('user_office_tools') }}"><i class="material-icons">build</i><span class="hide-on-small-and-down">Outils adhésion et ré-adhésion</span></a><i class="material-icons">chevron_right</i>
+<a href="{{ path('admin') }}"><i class="material-icons">build</i>&nbsp;Administration</a><i class="material-icons">chevron_right</i>
+<a href="{{ path('user_office_tools') }}"><i class="material-icons">folder_shared</i>&nbsp;Adhésion et ré-adhésion</a><i class="material-icons">chevron_right</i>
 <i class="material-icons">list</i>&nbsp;Liste des retardataires
 {% endblock %}
 
@@ -112,8 +113,8 @@
         <thead>
         <tr>
             <th data-col="o.member_number">#</th>
-            <th data-col="b.firstname">prénom</th>
-            <th data-col="b.lastname">nom</th>
+            <th data-col="b.firstname">Prénom</th>
+            <th data-col="b.lastname">Nom</th>
             <th data-col="r.date">Adhésion(s)</th>
             <th data-col="time">Compteur</th>
             <th>Note(s)</th>

app/Resources/views/beneficiary/confirm.html.twig
@@ -3,7 +3,8 @@
 {% block breadcrumbs %}
 {% if is_granted("IS_AUTHENTICATED_REMEMBERED") %}
     <a href="{{ path('homepage') }}"><i class="material-icons">home</i></a><i class="material-icons">chevron_right</i>
-    <a href="{{ path('user_office_tools') }}"><i class="material-icons">build</i>&nbsp;Outils adhésion et ré-adhésion</a><i class="material-icons">chevron_right</i>
+    <a href="{{ path('admin') }}"><i class="material-icons">build</i>&nbsp;Administration</a><i class="material-icons">chevron_right</i>
+    <a href="{{ path('user_office_tools') }}"><i class="material-icons">folder_shared</i>&nbsp;Adhésion et ré-adhésion</a><i class="material-icons">chevron_right</i>
     <a href="{{ path('find_member_number') }}"><i class="material-icons">search</i>&nbsp;Recherche numéro d'adhérent</a><i class="material-icons">chevron_right</i>
     <i class="material-icons">check</i>&nbsp;Trouvé
 {% endif %}

app/Resources/views/beneficiary/edit_beneficiary.html.twig
@@ -4,7 +4,8 @@
 
 {% block breadcrumbs %}
 <a href="{{ path('homepage') }}"><i class="material-icons">home</i></a><i class="material-icons">chevron_right</i>
-<a href="{{ path('user_office_tools') }}"><i class="material-icons">build</i>&nbsp;Outils adhésion et ré-adhésion</a> <i class="material-icons">chevron_right</i>
+<a href="{{ path('admin') }}"><i class="material-icons">build</i>&nbsp;Administration</a><i class="material-icons">chevron_right</i>
+<a href="{{ path('user_office_tools') }}"><i class="material-icons">folder_shared</i>&nbsp;Adhésion et ré-adhésion</a><i class="material-icons">chevron_right</i>
 <a href="{{ path('member_show', { 'member_number': beneficiary.membership.memberNumber }) }}"><i class="material-icons">person</i>&nbsp;{{ beneficiary.membership.autocompleteLabel }}</a><i class="material-icons">chevron_right</i>
 <i class="material-icons">edit</i>&nbsp;Editer un bénéficiaire
 {% endblock %}

app/Resources/views/beneficiary/find_member_number.html.twig
@@ -3,7 +3,8 @@
 {% block breadcrumbs %}
 {% if is_granted("IS_AUTHENTICATED_REMEMBERED") %}
     <a href="{{ path('homepage') }}"><i class="material-icons">home</i></a><i class="material-icons">chevron_right</i>
-    <a href="{{ path('user_office_tools') }}"><i class="material-icons">build</i>&nbsp;Outils adhésion et ré-adhésion</a><i class="material-icons">chevron_right</i>
+    <a href="{{ path('admin') }}"><i class="material-icons">build</i>&nbsp;Administration</a><i class="material-icons">chevron_right</i>
+    <a href="{{ path('user_office_tools') }}"><i class="material-icons">folder_shared</i>&nbsp;Adhésion et ré-adhésion</a><i class="material-icons">chevron_right</i>
     <i class="material-icons">search</i>&nbsp;Recherche numéro d'adhérent
 {% endif %}
 {% endblock %}

app/Resources/views/default/index.html.twig
@@ -150,16 +150,9 @@
                     </div>
                     <div class="col offset-xl3 xl6 m8 offset-m2 s12">
                         <div class="homebox">
-                            <h5><span class="white"><i class="material-icons">build</i>&nbsp;Action !</span></h5>
+                            <h5><span class="white"><i class="material-icons">build</i>&nbsp;Action</span></h5>
 
                             {{ render(controller("AppBundle:Code:homepageDashboard")) }}{# use include instead?#}
-
-                            {% if is_granted("access_tools", app.user) %}
-                                <a href="{{ path("user_office_tools") }}" class="waves-effect waves-light btn teal">
-                                    <i class="material-icons left">folder_shared</i>adhésion / ré-adhésion
-                                </a>
-                                <br>
-                            {% endif %}
 
                             {% if app.user.beneficiary and app.user.beneficiary.ownedCommissions | length %}
                                 <h5>Mes commissions</h5>

commit 24e502f34c614a21f8c13b946f91836d69ba9103

Fix wrong security role introduced in commit 618f34b3
No role here as this endpoint is used to create user account
from a link sent by email to anonymous beneficiaries (= new
member without valid account)

src/AppBundle/Controller/MembershipController.php
@@ -611,7 +611,6 @@ class MembershipController extends Controller
      *
      * @Route("/new", name="member_new")
      * @Method({"GET", "POST"})
-     * @Security("has_role('ROLE_SUPER_ADMIN')")
      */
     public function newAction(Request $request)
     {