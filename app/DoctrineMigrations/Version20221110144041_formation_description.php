449b5f458dbfc0a5b98850b92d3d0ea3fb01b30a
Raphael Odini <raphodn@users.noreply.github.com>
2022-11-13 13:09:22 +0100

    Fix migration file

--- app/DoctrineMigrations/Version20221110144041_formation_description.php
+++ app/DoctrineMigrations/Version20221110144041_formation_description.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221107144041 extends AbstractMigration
+final class Version20221110144041 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -23,9 +23,6 @@ final class Version20221107144041 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE formation ADD description LONGTEXT DEFAULT NULL');
-        $this->addSql('ALTER TABLE membership DROP FOREIGN KEY FK_86FFD28589BE4D2E');
-        $this->addSql('DROP INDEX IDX_86FFD28589BE4D2E ON membership');
-        $this->addSql('ALTER TABLE membership DROP withdrawn_by_id, DROP withdrawn_date');
     }
 
     public function down(Schema $schema) : void
@@ -34,8 +31,5 @@ final class Version20221107144041 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE formation DROP description');
-        $this->addSql('ALTER TABLE membership ADD withdrawn_by_id INT DEFAULT NULL, ADD withdrawn_date DATE DEFAULT NULL');
-        $this->addSql('ALTER TABLE membership ADD CONSTRAINT FK_86FFD28589BE4D2E FOREIGN KEY (withdrawn_by_id) REFERENCES fos_user (id)');
-        $this->addSql('CREATE INDEX IDX_86FFD28589BE4D2E ON membership (withdrawn_by_id)');
     }
 }
