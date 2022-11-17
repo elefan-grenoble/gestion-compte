7753dcd9163fd5641d9032093500c469ae5f191f
Raphael Odini <raphodn@users.noreply.github.com>
2022-11-17 15:25:51 +0100

    Add validation checks

--- src/AppBundle/Controller/MembershipController.php
+++ src/AppBundle/Controller/MembershipController.php
@@ -856,32 +856,41 @@ class MembershipController extends Controller
             preg_match_all($re, $str, $matches, PREG_SET_ORDER, 0);
             if (count($matches) >= 2) {
                 $fromMember = $em->getRepository('AppBundle:Membership')->findOneBy(array("member_number" => $matches[0][1]));
-                if ($fromMember) {
-                    $destMember = $em->getRepository('AppBundle:Membership')->findOneBy(array("member_number" => $matches[1][1]));
-                    if ($destMember) {
-                        foreach ($fromMember->getBeneficiaries() as $beneficiary) {
-                            $destMember->addBeneficiary($beneficiary); //in
-                            $fromMember->removeBeneficiary($beneficiary); //out
-                            $beneficiary->setMembership($destMember);
-                            $em->persist($beneficiary);
-                        }
-                        $em->persist($destMember);
-                        $em->flush();
-                        $fromMember->setMainBeneficiary(null);
-                        $em->remove($fromMember);
-                        $em->flush();
-
-                        $session->getFlashBag()->add('success', 'Les deux adhérents ont bien été fusionnés');
-
-                        return $this->redirectToShow($destMember);
-                    } else {
-                        $session->getFlashBag()->add('error', 'impossible de trouver le compte de destination');
-                    }
+                if (!$fromMember) {
+                    $session->getFlashBag()->add('error', 'Impossible de trouver le compte à lier.');
                 } else {
-                    $session->getFlashBag()->add('error', 'impossible de trouver le compte à lier');
+                    $destMember = $em->getRepository('AppBundle:Membership')->findOneBy(array("member_number" => $matches[1][1]));
+                    if (!$destMember) {
+                        $session->getFlashBag()->add('error', 'Impossible de trouver le compte de destination.');
+                    } else {
+                        if ($fromMember == $destMember) {
+                            $session->getFlashBag()->add('error', 'Impossible de joindre deux comptes identiques.');
+                        } else if ($fromMember->getBeneficiaries()->count() >= $this->getParameter('maximum_nb_of_beneficiaries_in_membership')) {
+                            $session->getFlashBag()->add('error', 'Le compte à lier a déjà le nombre maximum de bénéficiaires.');
+                        }else if ($destMember->getBeneficiaries()->count() >= $this->getParameter('maximum_nb_of_beneficiaries_in_membership')) {
+                            $session->getFlashBag()->add('error', 'Le compte de destination a déjà le nombre maximum de bénéficiaires.');
+                        } else if ($fromMember->getBeneficiaries()->count() + $destMember->getBeneficiaries()->count() > $this->getParameter('maximum_nb_of_beneficiaries_in_membership')) {
+                            $session->getFlashBag()->add('error', 'La somme des bénéficiaires du compte à lier (' . $destMember->getBeneficiaries()->count() . ') et du compte de destination (' . $fromMember->getBeneficiaries()->count() . ') dépasse le nombre maximum de bénéficiaires.');
+                        } else {
+                            foreach ($fromMember->getBeneficiaries() as $beneficiary) {
+                                $destMember->addBeneficiary($beneficiary); //in
+                                $fromMember->removeBeneficiary($beneficiary); //out
+                                $beneficiary->setMembership($destMember);
+                                $em->persist($beneficiary);
+                            }
+                            $em->persist($destMember);
+                            $em->flush();
+                            $fromMember->setMainBeneficiary(null);
+                            $em->remove($fromMember);
+                            $em->flush();
+
+                            $session->getFlashBag()->add('success', 'Les deux comptes adhérents ont bien été fusionnés !');
+
+                            return $this->redirectToShow($destMember);
+                        }
+                    }
                 }
             }
-
         }
 
         $members = $em->getRepository('AppBundle:Membership')->findAll(); //todo exclude closed
