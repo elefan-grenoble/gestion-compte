2e605bfeb5a076014827bd183b2529e7756dbdfa
Raphael Odini <raphodn@users.noreply.github.com>
2022-12-03 19:06:55 +0100

    Improve shiftexemption list page

--- src/AppBundle/Form/ShiftExemptionType.php
+++ src/AppBundle/Form/ShiftExemptionType.php
@@ -4,6 +4,7 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ShiftExemptionType extends AbstractType
@@ -13,8 +14,10 @@ class ShiftExemptionType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name');
-    }/**
+        $builder->add('name', TextType::class, array('label' => 'Nom', 'required' => true));
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function configureOptions(OptionsResolver $resolver)
@@ -31,6 +34,5 @@ class ShiftExemptionType extends AbstractType
     {
         return 'appbundle_shiftexemption';
     }
-
 
 }
