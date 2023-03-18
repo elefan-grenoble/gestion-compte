244d10afa01022460c1e12be013363bc02ceed9a
Raphael Odini <raphodn@users.noreply.github.com>
2023-03-19 00:03:14 +0100

    Compte épargne : homogénéiser les droits d'annulation d'un créneau (entre membre & admin) (#792)
    * Improve canBookShift to also send error message
    
    * Simplify freeShiftAction (time_log_saving rules moved to canBookShift)
    
    * Adapt canFreeShift to be called from admin
    
    * Improve messages to admin when freeing shift

--- src/AppBundle/Controller/ShiftController.php
+++ src/AppBundle/Controller/ShiftController.php
@@ -250,6 +250,7 @@ class ShiftController extends Controller
     public function freeShiftAction(Request $request, Shift $shift)
     {
         $session = new Session();
+        $current_app_user = $this->get('security.token_storage')->getToken()->getUser();
 
         $this->denyAccessUnlessGranted(ShiftVoter::FREE, $shift);
 
@@ -258,28 +259,11 @@ class ShiftController extends Controller
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            if ($shift->isFixe()) {
-                $session->getFlashBag()->add("error", "Impossible d'annuler un créneau fixe !");
+            // check if beneficiary can free this shift
+            $shift_can_be_freed = $this->get('shift_service')->canFreeShift($current_app_user->getBeneficiary(), $shift);
+            if (!$shift_can_be_freed['result']) {
+                $session->getFlashBag()->add("error", $shift_can_be_freed['message'] || "Impossible d'annuler ce créneau.");
                 return $this->redirectToRoute("homepage");
-            }
-            if (!$shift->getShifter()) {
-                $session->getFlashBag()->add("error", "Impossible de libérer le créneau car il n'est actuellement pas réservé.");
-                return $this->redirectToRoute("homepage");
-            }
-
-            if ($this->use_time_log_saving) {
-                $member = $shift->getShifter()->getMembership();
-                $member_saving_now = $member->getSavingTimeCount();
-                // the saving account must have the necessary amount
-                if ($shift->getDuration() > $member_saving_now) {
-                    $session->getFlashBag()->add("warning", "Impossible de libérer le créneau car la capacité de votre compteur épargne est trop faible.");
-                    return $this->redirectToRoute("homepage");
-                }
-                // check if there is a time_in_advance rule
-                if (isset($this->time_log_saving_shift_free_min_time_in_advance_days) && $shift->isBefore($this->time_log_saving_shift_free_min_time_in_advance_days . ' days')) {
-                    $session->getFlashBag()->add("warning", "Impossible de libérer le créneau car il a lieu dans moins de " . $this->time_log_saving_shift_free_min_time_in_advance_days . " jours.");
-                    return $this->redirectToRoute("homepage");
-                }
             }
 
             // store shift beneficiary & reason (before shift free())
@@ -315,43 +299,48 @@ class ShiftController extends Controller
      */
     public function freeShiftAdminAction(Request $request, Shift $shift)
     {
+        $session = new Session();
+        $current_app_user = $this->get('security.token_storage')->getToken()->getUser();
+
         $this->denyAccessUnlessGranted(ShiftVoter::FREE, $shift);
 
         $form = $this->createShiftFreeAdminForm($shift);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            if (!$shift->getShifter()) {
-                $success = false;
-                $message = "Impossible de libérer le créneau car il n'est actuellement pas réservé.";
-            } else {
-                // store shift beneficiary & reason (before shift free())
-                $beneficiary = $shift->getShifter();
-                $fixe = $shift->isFixe();
-                $reason = $form->get("reason")->getData();
-
-                // shouldn't happen: in the UI, you need to first invalidate a shift before being able to free it
-                $wasCarriedOut = $shift->getWasCarriedOut() == 1;
-                if ($wasCarriedOut) {
-                    $shift->invalidateShiftParticipation();
-                }
-
-                // free shift
-                $shift->free($reason);
-
-                $em = $this->getDoctrine()->getManager();
-                $em->persist($shift);
-                $em->flush();
-
-                $dispatcher = $this->get('event_dispatcher');
-                if ($wasCarriedOut) {
-                    $dispatcher->dispatch(ShiftInvalidatedEvent::NAME, new ShiftInvalidatedEvent($shift, $beneficiary));
-                }
-                $dispatcher->dispatch(ShiftFreedEvent::NAME, new ShiftFreedEvent($shift, $beneficiary, $fixe, $reason));
-
-                $success = true;
-                $message = "Le créneau a bien été libéré !";
+            // check if shift can be freed
+            $shift_can_be_freed = $this->get('shift_service')->canFreeShift($shift->getShifter(), $shift, true);
+            if (!$shift_can_be_freed['result']) {
+                $session->getFlashBag()->add("error", $shift_can_be_freed['message'] || "Impossible d'annuler ce créneau.");
+                return $this->redirectToRoute("homepage");
             }
+
+            // store shift beneficiary & reason (before shift free())
+            $beneficiary = $shift->getShifter();
+            $fixe = $shift->isFixe();
+            $reason = $form->get("reason")->getData();
+
+            // shouldn't happen: in the UI, you need to first invalidate a shift before being able to free it
+            $wasCarriedOut = $shift->getWasCarriedOut() == 1;
+            if ($wasCarriedOut) {
+                $shift->invalidateShiftParticipation();
+            }
+
+            // free shift
+            $shift->free($reason);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($shift);
+            $em->flush();
+
+            $dispatcher = $this->get('event_dispatcher');
+            if ($wasCarriedOut) {
+                $dispatcher->dispatch(ShiftInvalidatedEvent::NAME, new ShiftInvalidatedEvent($shift, $beneficiary));
+            }
+            $dispatcher->dispatch(ShiftFreedEvent::NAME, new ShiftFreedEvent($shift, $beneficiary, $fixe, $reason));
+
+            $success = true;
+            $message = "Le créneau a bien été libéré !";
         } else {
             $success = false;
             $message = "Une erreur s'est produite... Impossible de libérer le créneau. " . (string) $form->getErrors(true, false);
@@ -374,7 +363,6 @@ class ShiftController extends Controller
                 return new JsonResponse(array('message'=>$message), 400);
             }
         } else {
-            $session = new Session();
             $session->getFlashBag()->add($success ? 'success' : 'error', $message);
             $referer = $request->headers->get('referer');
             return new RedirectResponse($referer);
