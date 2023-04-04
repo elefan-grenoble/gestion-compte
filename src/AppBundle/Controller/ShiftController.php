f385b1314754a990f2f88f3f37a25d4cb7554a65
Raphael Odini <raphodn@users.noreply.github.com>
2023-04-04 12:07:05 +0200

    If forbid_own_shift_validate_admin, then non-admin cannot (in)validate their own shift via admin

--- src/AppBundle/Controller/ShiftController.php
+++ src/AppBundle/Controller/ShiftController.php
@@ -35,13 +35,15 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 class ShiftController extends Controller
 {
     private $forbid_own_shift_free_admin;
+    private $forbid_own_shift_validate;
     private $use_fly_and_fixed;
     private $use_time_log_saving;
     private $time_log_saving_shift_free_min_time_in_advance_days;
 
-    public function __construct(bool $forbid_own_shift_free_admin, bool $use_fly_and_fixed, bool $use_time_log_saving, $time_log_saving_shift_free_min_time_in_advance_days)
+    public function __construct(bool $forbid_own_shift_free_admin, bool $forbid_own_shift_validate, bool $use_fly_and_fixed, bool $use_time_log_saving, $time_log_saving_shift_free_min_time_in_advance_days)
     {
         $this->forbid_own_shift_free_admin = $forbid_own_shift_free_admin;
+        $this->forbid_own_shift_validate = $forbid_own_shift_validate;
         $this->use_fly_and_fixed = $use_fly_and_fixed;
         $this->use_time_log_saving = $use_time_log_saving;
         $this->time_log_saving_shift_free_min_time_in_advance_days = $time_log_saving_shift_free_min_time_in_advance_days;
@@ -385,12 +387,22 @@ class ShiftController extends Controller
      */
     public function validateShiftAction(Request $request, Shift $shift)
     {
+        $session = new Session();
+        $current_app_user = $this->get('security.token_storage')->getToken()->getUser();
+
         $this->denyAccessUnlessGranted(ShiftVoter::VALIDATE, $shift);
 
         $form = $this->createShiftValidateInvalidateForm($shift);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $shifter_is_current_user = $current_app_user->getBeneficiary() == $shift->getShifter();
+            // check if admin user is allowed to (in)validate shift
+            if ($shifter_is_current_user && $this->forbid_own_shift_validate && !$this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+                $success = false;
+                $message = "Vous ne pouvez pas (in)valider votre propre créneau.";
+            }
+
             $em = $this->getDoctrine()->getManager();
             $validate = $form->get('validate')->getData() == 1;
             $current = $shift->getWasCarriedOut() == 1;
@@ -434,12 +446,11 @@ class ShiftController extends Controller
                 $modal = $this->forward('AppBundle\Controller\BookingController::showBucketAction', [
                     'bucket' => $bucket->getShiftWithMinId()
                 ])->getContent();
-                return new JsonResponse(array('message'=>$message, 'card' => $card, 'modal' => $modal), 200);
+                return new JsonResponse(array('message' => $message, 'card' => $card, 'modal' => $modal), 200);
             } else {
-                return new JsonResponse(array('message'=>$message), 400);
+                return new JsonResponse(array('message' => $message), 400);
             }
         } else {
-            $session = new Session();
             $session->getFlashBag()->add($success ? 'success' : 'error', $message);
             $referer = $request->headers->get('referer');
             return new RedirectResponse($referer);
