f294db771e95af8884adaec9b7991061cf384645
Raphael Odini <raphodn@users.noreply.github.com>
2023-05-06 19:03:12 +0200

    Rebase. Add confirmation modal

--- app/DoctrineMigrations/Version20230506151955_shift_free_log_created_at.php
+++ app/DoctrineMigrations/Version20230506151955_shift_free_log_created_at.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230426151955 extends AbstractMigration
+final class Version20230506151955 extends AbstractMigration
 {
     public function getDescription() : string
     {
