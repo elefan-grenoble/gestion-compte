1e4898b46b93aacc6703ca36b09648b8e06ad926
Raphael Odini <raphodn@users.noreply.github.com>
2023-06-15 21:05:22 +0200

    Nouvelle entité pour stocker les fermetures exceptionnelles (#889)
    * Create ClosingException entity
    
    * Create ClosingExceptionController & ClosingExceptionType
    
    * First working index & new views

---
+++ src/AppBundle/Controller/ClosingExceptionController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Entity\ClosingException;
+use AppBundle\Form\ClosingExceptionType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+
+/**
+ * ClosingException controller.
+ *
+ * @Route("admin/closingexceptions")
+ */
+class ClosingExceptionController extends Controller
+{
+    /**
+     * List all closing exceptions.
+     *
+     * @Route("/", name="admin_closingexception_index", methods={"GET"})
+     * @Security("has_role('ROLE_ADMIN')")
+     */
+    public function indexAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $closingExceptions = $em->getRepository('AppBundle:ClosingException')->findAll();
+
+        return $this->render('admin/closingexception/index.html.twig', array(
+            'closingExceptions' => $closingExceptions
+        ));
+    }
+
+    /**
+     * Add new closing exception.
+     *
+     * @Route("/new", name="admin_closingexception_new", methods={"GET","POST"})
+     * @Security("has_role('ROLE_ADMIN')")
+     */
+    public function newAction(Request $request)
+    {
+        $session = new Session();
+        $em = $this->getDoctrine()->getManager();
+        $current_user = $this->get('security.token_storage')->getToken()->getUser();
+
+        $closingException = new ClosingException();
+
+        $form = $this->createForm(ClosingExceptionType::class, $closingException);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $closingException->setCreatedBy($current_user);
+
+            $em->persist($closingException);
+            $em->flush();
+
+            $session->getFlashBag()->add('success', "La fermeture exceptionnelle a bien été crée !");
+            return $this->redirectToRoute('admin_closingexception_index');
+        }
+
+        return $this->render('admin/closingexception/new.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+}
---
+++ src/AppBundle/Entity/ClosingException.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * ClosingException
+ *
+ * @ORM\Table(name="closing_exception")
+ * @ORM\HasLifecycleCallbacks()
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ClosingExceptionRepository")
+ */
+class ClosingException
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @Assert\Date
+     *
+     * @ORM\Column(name="date", type="date")
+     */
+    private $date;
+
+    /**
+     * @var \DateTime
+     * 
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(name="created_by_id", referencedColumnName="id")
+     */
+    private $createdBy;
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function setCreatedAtValue()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return OpeningHour
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return OpeningHour
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set createdBy
+     *
+     * @param \AppBundle\Entity\User $createBy
+     *
+     * @return ClosingException
+     */
+    public function setCreatedBy(\AppBundle\Entity\User $user = null)
+    {
+        $this->createdBy = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get createdBy
+     *
+     * @return \AppBundle\Entity\User
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+}
