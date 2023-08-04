69eaa010af5387b4580f61a971aa7e5a9d32c413
Raphael Odini <raphodn@users.noreply.github.com>
2023-08-04 17:25:12 +0200

    Schedule: show week cycle only if cycle_type defined

---
+++ src/AppBundle/Service/PeriodService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AppBundle\Service;
+
+class PeriodService
+{
+    public function __construct()
+    {
+    }
+
+    public function getWeekCycleArray()
+    {
+        return ["A", "B", "C", "D"];
+    }
+}
