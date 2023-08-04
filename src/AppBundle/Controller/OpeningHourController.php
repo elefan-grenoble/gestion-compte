34ecdf494171cdd54e4452a40d7984704e01e6a1
Raphael Odini <raphodn@users.noreply.github.com>
2023-08-04 15:28:02 +0200

    Page to generate OpeningHour widget

--- src/AppBundle/Controller/OpeningHourController.php
+++ src/AppBundle/Controller/OpeningHourController.php
@@ -8,20 +8,22 @@ use AppBundle\Form\OpeningHourType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Form;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Session\Session;
 
 
 /**
- * OpeningHour controller.
+ * OpeningHour controller
  *
  * @Route("admin/openinghours")
  */
 class OpeningHourController extends Controller
 {
     /**
-     * List all opening hours.
+     * List all opening hours
      *
      * @Route("/", name="admin_openinghour_index", methods={"GET"})
      * @Security("has_role('ROLE_ADMIN')")
@@ -37,7 +39,7 @@ class OpeningHourController extends Controller
     }
 
     /**
-     * Add new opening hour.
+     * Add new opening hour
      *
      * @Route("/new", name="admin_openinghour_new", methods={"GET","POST"})
      * @Security("has_role('ROLE_ADMIN')")
@@ -71,7 +73,7 @@ class OpeningHourController extends Controller
     }
 
     /**
-     * Edit opening hour.
+     * Edit opening hour
      *
      * @Route("/edit/{id}", name="admin_openinghour_edit", methods={"GET","POST"})
      * @Security("has_role('ROLE_ADMIN')")
@@ -109,7 +111,7 @@ class OpeningHourController extends Controller
     }
 
     /**
-     * Delete opening hour.
+     * Delete opening hour
      *
      * @Route("/{id}", name="admin_openinghour_delete", methods={"DELETE"})
      * @Security("has_role('ROLE_ADMIN')")
@@ -131,6 +133,35 @@ class OpeningHourController extends Controller
         }
 
         return $this->redirectToRoute('admin_openinghour_index');
+    }
+
+    /**
+     * Opening hour widget generator
+     *
+     * @Route("/widget_generator", name="admin_openinghour_widget_generator", methods={"GET","POST"})
+     * @Security("has_role('ROLE_ADMIN')")
+     */
+    public function widgetGeneratorAction(Request $request)
+    {
+        $form = $this->createFormBuilder()
+            ->add('title', CheckboxType::class, array('required' => false, 'data' => true, 'label' => 'Afficher le titre du widget ?'))
+            ->add('generate', SubmitType::class, array('label' => 'Générer'))
+            ->getForm();
+
+        if ($form->handleRequest($request)->isValid()) {
+            $data = $form->getData();
+
+            $widgetQueryString = 'title='.($data['title'] ? 1 : 0);
+
+            return $this->render('admin/openinghour/widget_generator.html.twig', array(
+                'query_string' => $widgetQueryString,
+                'form' => $form->createView(),
+            ));
+        }
+
+        return $this->render('admin/openinghour/widget_generator.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 
     /**
