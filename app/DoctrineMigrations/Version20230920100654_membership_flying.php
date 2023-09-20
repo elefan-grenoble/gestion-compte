285d1ed15d96d2ac0823efac44de13694a78283f
Raphael Odini <raphodn@users.noreply.github.com>
2023-09-20 10:30:09 +0200

    Rename migration

--- app/DoctrineMigrations/Version20230920100654_membership_flying.php
+++ app/DoctrineMigrations/Version20230920100654_membership_flying.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230729100654 extends AbstractMigration
+final class Version20230920100654 extends AbstractMigration
 {
     public function getDescription() : string
     {
