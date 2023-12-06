77e0eb0717abf1fa85652caf0c4b0fba863756dd
Raphael Odini <raphodn@users.noreply.github.com>
2023-12-06 11:04:58 +0100

    Use startswith instead of equals

--- src/AppBundle/Helper/PlaceIP.php
+++ src/AppBundle/Helper/PlaceIP.php
@@ -12,13 +12,27 @@ class PlaceIP {
         $this->container = $container;
     }
 
+    /**
+     * If enable_place_local_ip_address_check is true & place_local_ip_address is set
+     * Then we check the client's IP against the IP list
+     */
     public function isLocationOk()
     {
-        $ip = $this->container->get('request_stack')->getCurrentRequest()->getClientIp();
         $checkIps = $this->container->getParameter('enable_place_local_ip_address_check');
-        $ips = $this->container->getParameter('place_local_ip_address');
-        $ips = explode(',', $ips);
 
-        return (isset($checkIps) and !$checkIps) or (isset($ip) and in_array($ip, $ips));
+        if (isset($checkIps) and $checkIps) {
+            $whitelist_ips = $this->container->getParameter('place_local_ip_address');
+            $whitelist_ips = explode(',', $whitelist_ips);
+            $current_ip = $this->container->get('request_stack')->getCurrentRequest()->getClientIp();
+
+            $current_ip_in_whitelist_ips = array_filter($whitelist_ips, function($whitelist_ip) use ($current_ip) {
+                return str_starts_with($current_ip, $whitelist_ip);
+            });
+            if (count($current_ip_in_whitelist_ips)) {
+                return True;
+            }
+        }
+
+        return False;
     }
 }
