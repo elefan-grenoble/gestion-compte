20c9392a63c0985bc617e9904f799563a5b9c419
sam <samuel.eyre@homail.fr>
2024-02-13 09:47:29 +0100

    add first e2e passing test

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://membres.yourcoop.local:8000',
+    viewportWidth: 1920,
+    viewportHeight: 1080,
   },
 });
