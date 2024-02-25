ccda8267ceab5c5014d2a05ded819b9fdd65f4c0
sam <samuel.eyre@homail.fr>
2024-02-25 14:18:05 +0100

    debug

--- cypress/e2e/keycloak/admin/admin1_can_login.cy.js
+++ cypress/e2e/keycloak/admin/admin1_can_login.cy.js
@@ -1,20 +1,11 @@
 // NO PERMANENT CHANGE TO DATABASE
 
-
-// temporarily disable uncaught exception handling
 import {login} from "../keycloak_reusables.cytools";
 
 let keycloakUrl = Cypress.env('KEYCLOAK_URL')
 
-// check if there is no reference error
-if (keycloakUrl === undefined) {
-    keycloakUrl = 'http://localhost:8080' // default value need for CI
-}
 
-if (!['http://localhost:8080', 'http://keycloak:8080'].includes(keycloakUrl)) {
-    keycloakUrl = 'http://localhost:8080' // default value need for CI
-}
-
+// temporarily disable uncaught exception handling
 Cypress.on('uncaught:exception', (err, runnable) => {
     return false
 })
@@ -23,23 +14,39 @@ describe('admin1 can login', function () {
     it('admin story', function () {
 
         cy.visit("/")
+        cy.get('#login').click()
+
+        cy.visit("/")
+        cy.get('#login').click()
+
         cy.origin(keycloakUrl, () => {
-            cy.get('#username').type('test', {force: true})
+            cy.log("fill in the login form")
+            cy.get('#username').type('admin1', {force: true})
+            cy.get('#password').type('password', {force: true})
+
+            // submit
+            cy.get('#kc-login').click()
+
+            cy.location().then((location) => {
+                if (location !== null && location.origin === keycloakUrl) {
+                    cy.get('#kc-login').click()
+                } else {
+                    cy.log("not asked for access to user data")
+                }
+            })
         })
 
-        // login(keycloakUrl, "admin1", "password")
-        //
-        // cy.log('home page banner contains "admin"')
-        // cy.get('[data-cy=home_welcome_message]').contains('admin')
-        //
-        // cy.log('go to settings page')
-        // cy.get('[data-cy=settings_link]').click()
-        //
-        // cy.log('display informations')
-        // cy.get('[data-cy=open_my_informations]').click()
-        //
-        // cy.log('check if role_admin in the page')
-        // cy.get('[data-cy=user_roles_container]').contains('ROLE_ADMIN', {timeout: 2000})
+        cy.log('home page banner contains "admin"')
+        cy.get('[data-cy=home_welcome_message]').contains('admin')
+
+        cy.log('go to settings page')
+        cy.get('[data-cy=settings_link]').click()
+
+        cy.log('display informations')
+        cy.get('[data-cy=open_my_informations]').click()
+
+        cy.log('check if role_admin in the page')
+        cy.get('[data-cy=user_roles_container]').contains('ROLE_ADMIN', {timeout: 2000})
 
     })
 })
