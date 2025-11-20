4648460e8c607da5d63c5baf352adee444c30a98
njean42 <gunoi@njean.me>
2025-11-20 08:21:45 +0100

    fix: Configuration de la locale pour les dates en français.

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ use App\Kernel;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
+setlocale(LC_TIME, "fr_FR.UTF8");
+
 require dirname(__DIR__).'/config/bootstrap.php';
 
 if ($_SERVER['APP_DEBUG']) {
